c43e2618ffcbb72d211a3fb1777e7d39745c16d0
deymeson56 <76841432+deymeson56@users.noreply.github.com>
2021-02-21 01:12:28 -0300

    Create database.java

---
+++ database/database.java
@@ -0,0 +1,9 @@
+//Usando linguagens mesclas para criptografia da script com databases em server online
+if
+echo:
+public static Connection getConnection(https url)
+public static Connection getConnection(tekbot url, Properties info)
+public static Connection getConnection(tekbot3.0 url, String user, String password)
+'password' = /tek-main/eixo/x/database `captation` : password
+sleep
+if
