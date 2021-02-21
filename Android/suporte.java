c462163ef32669d46ab64bcd06bb7f93a7eaa6ee
deymeson56 <76841432+deymeson56@users.noreply.github.com>
2021-02-21 01:08:25 -0300

    Create suporte.java

---
+++ Android/suporte.java
@@ -0,0 +1,19 @@
+
+/*
+
+ * Classname             (Tek1.0)
+
+ *
+
+ * Version information   (1.0)
+
+ *
+
+ * Date                  (21/02,01:09)
+
+ *
+
+ * author                (TEKASHI)
+
+ * Copyright notice      (bot com suporte a Android/Java)
+
