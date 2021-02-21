2d4e84bafae7286b03ba25f8e89c607a4f8470f0
deymeson56 <76841432+deymeson56@users.noreply.github.com>
2021-02-21 01:54:03 -0300

    Delete database directory

--- database/database.java
+++
@@ -1,9 +0,0 @@
-//Usando linguagens mesclas para criptografia da script com databases em server online
-if
-echo:
-public static Connection getConnection(https url)
-public static Connection getConnection(tekbot url, Properties info)
-public static Connection getConnection(tekbot3.0 url, String user, String password)
-'password' = /tek-main/eixo/x/database `captation` : password
-sleep
-if
